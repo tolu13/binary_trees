251b2633b9d862e987bae173dac0387178e9f936
tolu13 <tolulopeolanrewaju8@gmail.com>
2024-01-04 12:19:02 -0800

    binary tree

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -24,7 +24,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	else
 	{
 		new_node->left = parent->left;
-		parent->left =new_node;
+		parent->left = new_node;
 		new_node->left->parent = new_node;
 	}
 	return (new_node);
